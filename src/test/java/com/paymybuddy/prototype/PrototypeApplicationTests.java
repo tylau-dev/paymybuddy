caa129d545b3a337a5edc20f5014148a73b55b5e
tylau-dev <lautsyyeung@gmail.com>
2022-09-01 16:59:58 +0200

    spring boot init

---
+++ src/test/java/com/paymybuddy/prototype/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
